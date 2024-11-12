9928c96e65766e9de5434aee97343988a6a205bb
harjotbasota <harjotsinghbasota@gmail.com>
2024-11-12 12:40:47 -0500

    skills componenet

--- scripts/renderSkills.js
+++ scripts/renderSkills.js
@@ -1,4 +1,14 @@
-const renderSkills = ()=>{
-    console.log('render skills is invoked');
-    document.getElementById('skills').innerHTML = <div> hi from skills</div>
+const renderSkills = async ()=>{
+    const fetchSkills = await fetch('./data/skillsData.json');
+    const skillsData = await fetchSkills.json();
+    const fetchSkillsContainer = await fetch('./pages/skillsContainer.html');
+    const skillsContainer = await fetchSkillsContainer.text();
+    Object.entries(skillsData).map((skillObject)=>{
+        const sectionHeader = skillObject[0];
+        console.log("section header:",sectionHeader);
+        Object.entries(skillObject[1]).map((skill)=>{
+            console.log("skill Name:",skill[0]);
+            console.log("skill image src: ", skill[1]);
+        })
+    })
 }
